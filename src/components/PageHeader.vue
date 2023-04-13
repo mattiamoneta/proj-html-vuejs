778cf2f6875101e4bc97af0677378f5b47b2b8bf
mattiamoneta <in.mattiamoneta@gmail.com>
2023-04-13 17:44:21 +0200

    Final Commit

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -18,7 +18,7 @@ export default {
         titleSmall: String,
         titleBigWhite: String,
         titleBigYellow: String,
-        jumboHeight: Number
+        jumboHeight: String
     }
 }
 </script>
